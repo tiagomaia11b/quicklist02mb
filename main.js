b9a9d2a573979e3d760e3dd1910222868dc9faea
tmaia5003 <tmaia5003@gmail.com>
2025-06-08 00:39:05 +0000

    adição da funcionalidade de aparecer e sumir a div.warning usando setTimeout

--- main.js
+++ main.js
@@ -40,6 +40,13 @@ function showItensList() {
 
 function removeItem(itemName) {
     const itemIndex = items.findIndex((item) => item.name === itemName)
+    const divWarning = document.querySelector(".warning")
+
+    divWarning.classList.remove("hide-warning")
+
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
 
     if (itemIndex !== -1) {
         items.splice(itemIndex, 1)
