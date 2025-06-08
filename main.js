ccd8df9bb7674541d88608d9083a9e8c6b4b37c8
tmaia5003 <tmaia5003@gmail.com>
2025-06-08 01:50:10 +0000

    adição do array no localStorage para persistir a informação e criação da função verifyLocalStorageItems para verificar sempre se já existe um array guardado no localStorage

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const items = []
+let items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
@@ -43,6 +43,8 @@ function showItensList() {
             </div>
         `
     })
+
+    localStorage.setItem("items", JSON.stringify(items))
 }
 
 function removeItem(itemName) {
@@ -71,3 +73,14 @@ function checkItem(itemName) {
 function addHideWarningClass() {
     document.querySelector(".warning").classList.add("hide-warning")
 }
+
+function verifyLocalStorageItems(){
+    const localStorageItems = localStorage.getItem("items")
+
+    if(localStorageItems) {
+        items = JSON.parse(localStorageItems)
+        showItensList()
+    }
+}
+
+verifyLocalStorageItems()
