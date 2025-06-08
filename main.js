0d224e31aa17cca7803837b8e8d3c847c25c1e16
tmaia5003 <tmaia5003@gmail.com>
2025-06-08 00:20:18 +0000

    criação da função showItemlist() para exibir os itens dentro da section.list

--- main.js
+++ main.js
@@ -11,4 +11,29 @@ function addItem() {
     items.push(item)
 
     document.querySelector("#item").value = ""
+
+    showItensList()
+}
+
+function showItensList() {
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent = ""
+
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+         <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}">
+                    <div class="custom-checkbox">
+                         <img src="./assets/checked.svg" alt="checked">
+                    </div>
+                    <label for="item-${index}">${item.name}</label>
+                </div>
+                
+                <button>
+                  <img src="./assets/trash-icon.svg" alt="trash icon">
+                </button>
+            </div>
+        `
+    })
 }
