70a62f0e2f106c8fc1d14700368825ebcd304326
tmaia5003 <tmaia5003@gmail.com>
2025-06-08 01:34:30 +0000

    criação da função addHideWarningClass para esconder a div.warning ao clicar no botão de X

--- main.js
+++ main.js
@@ -64,7 +64,10 @@ function removeItem(itemName) {
 
 function checkItem(itemName) {
     const item = items.find((item) => item.name === itemName)
-    
     item.checked = !item.checked
     showItensList()
+}
+
+function addHideWarningClass() {
+    document.querySelector(".warning").classList.add("hide-warning")
 }
